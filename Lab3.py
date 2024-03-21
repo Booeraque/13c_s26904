9048e7f8e270874ecdd70eb4d2a2e35fa0ba99f8
s26904 <s26904@pjwstk.edu.pl>
2024-03-21 14:46:43 +0100

    Completed the Task 3
    Used the __init__ function to set up the range and task3() function to generate the list of squares

--- Lab3.py
+++ Lab3.py
@@ -15,6 +15,18 @@ def task2():
 
 
 # Task 3
+# Classes
+class SquareGenerator:
+    start = 0
+    end = 0
+
+    def __init__(self):
+        self.start = int(input("Enter the start of the range: "))
+        self.end = int(input("Enter the end of the range: "))
+
+    def task3(self):
+        squaresList = [number * number for number in range(self.start, self.end + 1)]
+        print(squaresList)
 
 
 # Task 4
@@ -40,5 +52,5 @@ def task2():
 
 # Test the function
 if __name__ == "__main__":
-    result = task2()
-    print(result)
+    sq1 = SquareGenerator()
+    sq1.task3()
